61f4c04803023ace3f339555f7e6e21d04ba0492
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2025-08-25 15:28:32 +0200

    chore: change id type to uuid, adjust controller, service and repository types where needed

--- src/main/kotlin/pl/umcs/picto3/gameconfig/GameConfigRepository.kt
+++ src/main/kotlin/pl/umcs/picto3/gameconfig/GameConfigRepository.kt
@@ -1,6 +1,7 @@
 package pl.umcs.picto3.gameconfig
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
 
-interface GameConfigRepository : JpaRepository<GameConfig, Long> {
+interface GameConfigRepository : JpaRepository<GameConfig, UUID> {
 }
--- src/main/kotlin/pl/umcs/picto3/symbol/SymbolPlacementRepository.kt
+++ src/main/kotlin/pl/umcs/picto3/symbol/SymbolPlacementRepository.kt
@@ -1,7 +1,8 @@
 package pl.umcs.picto3.symbol
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
 
-interface SymbolPlacementRepository : JpaRepository<SymbolPlacement, Long> {
-    fun findByRowIndexAndColumnIndexAndSymbolId(rowIndex: Short, columnIndex: Short, symbolId: Long): SymbolPlacement?
+interface SymbolPlacementRepository : JpaRepository<SymbolPlacement, UUID> {
+    fun findByRowIndexAndColumnIndexAndSymbolId(rowIndex: Short, columnIndex: Short, symbolId: UUID): SymbolPlacement?
 }
