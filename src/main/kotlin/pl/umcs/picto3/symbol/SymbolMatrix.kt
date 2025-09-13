9a20bc3fe432bf9d70f7f9b336aa5137d1b743ae
mikolaj-martyna <mikolaj.martyna.01@gmail.com>
2025-09-13 20:32:53 +0200

    fix: adjust fetch type

--- src/main/kotlin/pl/umcs/picto3/symbol/SymbolMatrix.kt
+++ src/main/kotlin/pl/umcs/picto3/symbol/SymbolMatrix.kt
@@ -2,6 +2,7 @@ package pl.umcs.picto3.symbol
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -23,6 +24,6 @@ data class SymbolMatrix(
     @Column(name = "column_size")
     val columnSize: Short,
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     val symbolPlacements: List<SymbolPlacement>
 )
