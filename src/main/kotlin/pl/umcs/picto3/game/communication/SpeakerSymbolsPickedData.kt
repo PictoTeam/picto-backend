8f22d1f67ebbd3242bc3da3b9b7cc3704698fccd
kamilgolawski <kamilgolawski3@icloud.com>
2025-09-14 19:36:05 +0200

    removed round id

--- src/main/kotlin/pl/umcs/picto3/game/communication/SpeakerSymbolsPickedData.kt
+++ src/main/kotlin/pl/umcs/picto3/game/communication/SpeakerSymbolsPickedData.kt
@@ -3,7 +3,6 @@ package pl.umcs.picto3.game.communication
 import java.util.UUID
 
 data class SpeakerSymbolsPickedData(
-    val roundId: UUID,
     val symbolsIds: List<UUID>,
     val speakerResponseTime: Int,
 )
